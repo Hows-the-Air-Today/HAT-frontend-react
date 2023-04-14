9ec5955a5989513cf1fe263d94d468174e679805
김정훈 <88875539+jhva@users.noreply.github.com>
2023-04-14 16:27:50 +0900

    ✨ feat(HAT-220):  settings bottom bar (#5)
    * ➕ build(HAT-222): react-icon install
    
    * Revert "➕ build(HAT-222): react-icon install"
    
    This reverts commit 7e757b315dc1a6ef3436f62822c25b306aecd9fb.
    
    * Revert "Revert "➕ build(HAT-222): react-icon install""
    
    This reverts commit 7834e6a40e1eb25dc3446f2420244a8eae87a29b.
    
    * 🚑 fix: eslint build 오류 코드 포맷팅 수정
    
    ---------
    
    Co-authored-by: Hyyena <70932170+Hyyena@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RealTimePage from "components/pages/AirQuality/RealTimePage";
 import LoginPage from "components/pages/Auth/Login/LoginPage";
 import RegisterPage from "components/pages/Auth/Register/RegisterPage";
-
-import MainPage from "./components/pages/Main/MainPage";
+import CommunityPage from "components/pages/Community/CommunityPage";
+import MainPage from "components/pages/Main/MainPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/" element={<MainPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/community" element={<CommunityPage />} />
         <Route path="/airquality" element={<RealTimePage />} />
       </Routes>
     </Router>
