26133b6d6107d3ea0c86ffd3c3f453bf396ceb2c
jhva <junghun8158@naver.com>
2023-04-27 04:32:07 +0900

    ✨feat(HAT-324): 기존 게시물조회 박스 컴포넌트화

--- src/components/UI/organisms/dropBox/DropBoxMenu.js
+++ src/components/UI/organisms/dropBox/DropBoxMenu.js
@@ -1,46 +1,76 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { RiMore2Fill } from "react-icons/ri";
 
 import { Menu, MenuItem } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
 
-// eslint-disable-next-line react/prop-types
-const DropBoxMenu = ({ postId, open, anchorEl, setAnchor }) => {
-  const options = [
-    { id: 1, title: "수정" },
-    { id: 2, title: "삭제" },
-  ];
+// eslint-disable-next-line import/extensions
 
+const DropBoxMenu = ({ option, setOpen, handleOpenClick }) => {
   const ITEM_HEIGHT = 48;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const boxMenu = Boolean(anchorEl);
+  const ref = useRef(null);
   const handleClose = () => {
-    setAnchor(null);
+    console.log("adsf");
   };
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setAnchorEl(null);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
   return (
-    <Menu
-      id="long-menu"
-      MenuListProps={{
-        "aria-labelledby": "long-button",
-      }}
-      open={open}
-      anchorEl={anchorEl}
-      onClose={handleClose}
-      PaperProps={{
-        style: {
-          maxHeight: ITEM_HEIGHT * 4.5,
-          width: "20ch",
-        },
-      }}
-    >
-      {options?.map((option) => (
-        <MenuItem
-          onClick={() => {
-            // onClickSelect(option.title);
-            console.log(postId);
-          }}
-          selected={null}
-        >
-          <p>{option.title}</p>
-        </MenuItem>
-      ))}
-    </Menu>
+    <div ref={ref}>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={boxMenu ? "long-menu" : undefined}
+        aria-expanded={boxMenu ? "true" : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <RiMore2Fill />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          "aria-labelledby": "long-button",
+        }}
+        anchorEl={anchorEl}
+        open={boxMenu}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: "20ch",
+          },
+        }}
+      >
+        {option.map((options, index) => (
+          <MenuItem
+            key={index}
+            onClick={() => {
+              handleOpenClick(options.title);
+              setAnchorEl(null);
+            }}
+          >
+            {options?.title}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
   );
 };
 
