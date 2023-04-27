7584b4769c641ee9bf9a1bdfd9550f08d594ee5d
Hyyena <sjtxm0320@gmail.com>
2023-04-27 13:04:58 +0900

    ✨ feat: 하단 메뉴바 수정
    - 아이콘 수정
    - 아이콘에 해당하는 페이지 활성화 효과 추가

--- src/components/UI/organisms/Navigation/BottomMenuBar.tsx
+++ src/components/UI/organisms/Navigation/BottomMenuBar.tsx
@@ -1,18 +1,64 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import { Icon } from "./icons";
 import { StyledBottomMenuBar, ContainerBox } from "./styles/styles";
 
 const BottomMenuBar = (): JSX.Element => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const homeIndex = Icon.findIndex((item) => item.title === "home");
+  const [activeIcon, setActiveIcon] = useState(homeIndex);
+
+  const [showBottomMenuBar, setShowBottomMenuBar] = useState(true);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (currentScrollPosition > scrollPosition) {
+      // 하단 스크롤 시 하단바 숨김
+      setShowBottomMenuBar(false);
+    } else {
+      // 상단 스크롤 시 하단바 표시
+      setShowBottomMenuBar(true);
+    }
+
+    setScrollPosition(currentScrollPosition);
+  };
+
+  const handleNavigation = (path: string, index: number) => {
+    navigate(path);
+    setActiveIcon(index);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition, handleScroll]);
+
+  useEffect(() => {
+    const activeIndex = Icon.findIndex(
+      (item) => item.path === location.pathname
+    );
+    setActiveIcon(activeIndex);
+  }, [location.pathname]);
 
   return (
-    <StyledBottomMenuBar>
+    <StyledBottomMenuBar show={showBottomMenuBar}>
       <ContainerBox>
         {Icon.map((item, index) => (
-          <button onClick={() => navigate(item.path)} type="button">
-            {item.icon}
+          <button
+            /* eslint-disable-next-line react/no-array-index-key */
+            key={index}
+            onClick={() => handleNavigation(item.path, index)}
+            type="button"
+          >
+            {React.cloneElement(item.icon, {
+              color: index === activeIcon ? "#191f24" : "#80858A",
+            })}
           </button>
         ))}
       </ContainerBox>
