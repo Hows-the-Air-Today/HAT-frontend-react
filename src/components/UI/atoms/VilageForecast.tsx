a4b7dac31f557ad903590bdf34eaf82430750520
Hyyena <sjtxm0320@gmail.com>
2023-04-27 13:03:53 +0900

    ✨ feat(HAT-227): 실시간 날씨 조회 기능 추가
    - OpenAPI 단기 예보 조회 기능 추가
    - 위경도 -> 기상청 좌표 변환 코드 분리
    - 예보 발표 시간에 맞춰 조회 시간 설정
    - 하늘 상태에 따른 아이콘 추가
    - 기온에 따른 옷차림 아이콘 추가

---
+++ src/components/UI/atoms/VilageForecast.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { BsSnow2 } from "react-icons/bs";
+import { GiHeavyRain } from "react-icons/gi";
+import { IoIosPartlySunny, IoIosCloudy } from "react-icons/io";
+import { IoSunny } from "react-icons/io5";
+import { WiRain, WiRainMix } from "react-icons/wi";
+
+import { TextLoading } from "components/UI/atoms/TextLoading";
+
+interface VilageForecastProps {
+  x: number;
+  y: number;
+  dayIndex: number;
+  forecast: any;
+}
+
+const getWeatherIcon = (sky, pty) => {
+  if (pty === "1") {
+    return <GiHeavyRain />;
+  }
+  if (pty === "2") {
+    return <WiRainMix />;
+  }
+  if (pty === "3") {
+    return <BsSnow2 />;
+  }
+  if (pty === "4") {
+    return <WiRain />;
+  }
+  if (sky === "1") {
+    return <IoSunny />;
+  }
+  if (sky === "3") {
+    return <IoIosPartlySunny />;
+  }
+  if (sky === "4") {
+    return <IoIosCloudy />;
+  }
+  return null;
+};
+
+const VilageForecast: React.FC<VilageForecastProps> = ({
+  x,
+  y,
+  dayIndex,
+  forecast,
+}) => {
+  console.log("forecast", forecast);
+
+  if (!forecast) {
+    return <TextLoading />;
+  }
+
+  const { sky, pty, tmp } = forecast;
+
+  return forecast ? (
+    <div className="flex flex-row gap-1 align-middle text-center justify-center items-center">
+      {getWeatherIcon(sky, pty)}
+      <div className="align-middle">{tmp}°C</div>
+    </div>
+  ) : (
+    <TextLoading />
+  );
+};
+
+export default VilageForecast;
