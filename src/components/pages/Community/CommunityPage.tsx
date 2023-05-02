01932df8eeae01a93bbe0f055ee00479a89b5cd9
jhva <junghun8158@naver.com>
2023-05-02 11:31:59 +0900

    💡fix:주석제거

--- src/components/pages/Community/CommunityPage.tsx
+++ src/components/pages/Community/CommunityPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import InfiniteScroll from "react-infinite-scroller";
 
 import { useInfiniteQuery } from "@tanstack/react-query";
