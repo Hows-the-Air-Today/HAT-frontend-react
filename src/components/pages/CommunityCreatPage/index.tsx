872cb100d2c3165def38caa1f6b4fd3dd055ea9b
김정훈 <88875539+jhva@users.noreply.github.com>
2023-05-02 11:40:08 +0900

    🐛 fix(HAT-391): 게시판조회 무한스크롤 로딩추가 (#33)
    * ✨feat: 게시판조회 무한스크롤 로딩추가
    
    * 💡fix:주석제거

--- src/components/pages/CommunityCreatPage/index.tsx
+++ src/components/pages/CommunityCreatPage/index.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useState } from "react";
 import * as Ai from "react-icons/ai";
 import { useLocation, useNavigate } from "react-router-dom";
 
-import axios from "axios";
 import { useRecoilState } from "recoil";
 
 import { postCreateAndUpdate } from "../../../api/community";
